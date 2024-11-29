5f781d649c49e0c671211855bd37d80581d2a784
Chunhawit <pupz28@gmail.com>
2024-11-30 00:07:39 +0100

    added function to add and remove task

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue'
 const name = ref('John Doe')
 const status = ref('active')
 const tasks = ref(['Task One', 'Task Two', 'Task Three'])
+const newTask = ref('')
 
 const toggleStatus = () => {
   if (status.value === 'active') {
@@ -13,6 +14,17 @@ const toggleStatus = () => {
   } else {
     status.value = 'active'
   }
+}
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value)
+    newTask.value = ''
+  }
+}
+
+const deleteTask = index => {
+  tasks.value.splice(index, 1)
 }
 </script>
 
@@ -24,10 +36,21 @@ const toggleStatus = () => {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
   <!-- v-for -->
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="index">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
 
   <!-- v-bind -->
