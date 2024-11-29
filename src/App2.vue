253a0227d29e175e8f7814aba333aa9e64d06f3e
Chunhawit <pupz28@gmail.com>
2024-11-30 00:18:26 +0100

    moved code to App2.vue

--- src/App2.vue
+++ src/App2.vue
@@ -1,25 +1,41 @@
-<script>
-export default {
-  data() {
-    return {
-      name: 'John Doe',
-      status: 'active',
-      tasks: ['Read One Piece', 'Buy Milk', 'Slide IG'],
-      link: 'https://google.com',
-    }
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === 'active') {
-        this.status = 'pending'
-      } else if (this.status === 'pending') {
-        this.status = 'inactive'
-      } else {
-        this.status = 'active'
-      }
-    },
-  },
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const name = ref('John Doe')
+const status = ref('active')
+const tasks = ref(['Task One', 'Task Two', 'Task Three'])
+const newTask = ref('')
+
+const toggleStatus = () => {
+  if (status.value === 'active') {
+    status.value = 'pending'
+  } else if (status.value === 'pending') {
+    status.value = 'inactive'
+  } else {
+    status.value = 'active'
+  }
 }
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value)
+    newTask.value = ''
+  }
+}
+
+const deleteTask = index => {
+  tasks.value.splice(index, 1)
+}
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await response.json()
+    tasks.value = data.map(task => task.title)
+  } catch (error) {
+    console.log('Error fetching tasks')
+  }
+})
 </script>
 
 <template>
@@ -30,10 +46,21 @@ export default {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
   <!-- v-for -->
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="index">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
 
   <!-- v-bind -->
