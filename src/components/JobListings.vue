e58b700fb8ca211f3f6320340980144633a345e2
Chunhawit <pupz28@gmail.com>
2024-11-30 14:36:43 +0100

    using RouterLink instead of a-tag to perform better for SPA

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
 import JobListing from './JobListing.vue'
 import jobData from '@/jobs.json'
 
@@ -30,10 +31,10 @@ const jobs = ref(jobData)
     </div>
   </section>
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-    <a
-      href="/jobs"
+    <RouterLink
+      to="/jobs"
       class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-      >View All Jobs</a
-    >
+      >View All Jobs
+    </RouterLink>
   </section>
 </template>
